b564859d86a56a40c533eea6fc6688ba8a8ce381
Elias <eliasbekovski@gmail.com>
2022-10-16 22:42:48 -0300

    Login binance.

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
-{
-  "require": {
-    "vlucas/phpdotenv": "^3.5",
-    "jaggedsoft/php-binance-api": "^0.5.7",
-    "guzzlehttp/guzzle": "^6.3",
-  }
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Binance\API;
+use Binance\RateLimiter;
+
+Dotenv::create(__DIR__)->load();
+
+$key    = getenv('KEY');
+$secret = getenv('SECRET');
+
+$api = new API($key, $secret);
+$api->useServerTime();
+$api = new RateLimiter($api);
+
+try {
+    $balances = $api->balances();
+
+    $usdt = $balances['USDT'];
+
+    print_r($usdt);
+} catch (Exception $e) {
+
+    print("\033[91mERROR.\033[0m" . PHP_EOL);
+
+    print_r($e);
 }
